f471016af43cc6cccacfb4d296c964970310011d
amozzhevilov <91126756+amozzhevilov@users.noreply.github.com>
2024-10-04 10:03:06 +0500

    Lint (#1)
    reformat code for lint checks
    
    ---------
    
    Co-authored-by: Anatoly Mozzhevilov <anatoly.mozzhevilov@yandex.ru>

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import requests, json, os
+'''Send message to telegram about the availability of free warehouses on WB'''
+
+import json
+import os
 from datetime import datetime, timezone, timedelta
 from time import sleep
 
+import requests
 import wb
 
 # извлекаем токены из env
@@ -9,8 +13,11 @@ WB_TOKEN = os.getenv('WB_TOKEN')
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+# инициализируем класс для работы с WB API
+wb = wb.WB(WB_TOKEN)
+
 # фильтр по складам
-warehouses = '''
+WAREHOUSES = '''
 [
     {
         "warehouse": "Екатеринбург - Испытателей 14г|Екатеринбург - Перспективный 12/2|СЦ Екатеринбург 2 (Альпинистов)",
@@ -28,33 +35,39 @@ warehouses = '''
 '''
 
 def telegram_bot_sendtext(bot_message):
+    '''Send message to telegram.'''
+    bot_token = TELEGRAM_BOT_TOKEN
+    bot_chat_id = TELEGRAM_CHAT_ID
+    send_text = 'https://api.telegram.org/bot' + bot_token + \
+        '/sendMessage?chat_id=' + bot_chat_id + \
+        '&parse_mode=Markdown&text=' + bot_message
 
-   bot_token = TELEGRAM_BOT_TOKEN
-   bot_chatID = TELEGRAM_CHAT_ID
-   send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text, timeout=10)
 
-   response = requests.get(send_text)
-
-   return response.json()
-
+    return response.json()
 
 def send_message(result):
-    msg=''
+    '''Convert relust list to user message'''
+    msg=""
 
     for i in result:
-        msg += 'Склад: {}, дата: {}, коэф. {}.\n'.format(i['warehouseName'], datetime.fromisoformat(i['date']).strftime('%Y-%m-%d'), i['coefficient'])
-
+        msg += f"Склад: {i['warehouseName']}, "
+        msg += f"дата: {datetime.fromisoformat(i['date']).strftime('%Y-%m-%d')}, "
+        msg += f"коэф. {i['coefficient']}.\n"
     telegram_bot_sendtext(msg)
 
+def get_timedelta_to_now(date):
+    '''Return timedelta from date to now'''
+    return datetime.fromisoformat(date) - datetime.now(timezone.utc)
 
 def get_warehouse (result, warehouse, coefficients):
-    
-    for coefficient in coefficients: 
-        
+    '''Find warehouse by condition'''
+    for coefficient in coefficients:
+
         # коэф -1 - склад не работает
         if coefficient['coefficient'] == -1:
             continue
-        
+
         # проверяем, подходит ли нам тип упаковки
         if coefficient['boxTypeName'] not in warehouse['boxTypeName'].split('|'):
             continue
@@ -66,49 +79,50 @@ def get_warehouse (result, warehouse, coefficients):
 
         # проверяем коэф. склада, дату приемки и не добавляли ли мы склад ранее.
         if coefficient['coefficient'] <= warehouse['min_coefficient'] and \
-            datetime.fromisoformat(coefficient['date']) - datetime.now(timezone.utc) >= timedelta(days=warehouse['delay']) and \
+            get_timedelta_to_now(coefficient['date']) >= timedelta(days=warehouse['delay']) and \
             coefficient not in result:
-                result.append(coefficient)
+            result.append(coefficient)
 
-# преобразуем фильтр по складам в JSON
-warehouses = json.loads(warehouses)
+def main():
+    '''Main function'''
+    # преобразуем фильтр по складам в JSON
+    warehouses = json.loads(WAREHOUSES)
 
-# пустой лист для сохранения предыдущего состояния между циклами
-old_list = []
+    # пустой лист для сохранения предыдущего состояния между циклами
+    previous_list = []
 
-# инициализируем класс для работы с WB API
-wb = wb.wb(WB_TOKEN)
+    while True:
+        # извлекаем коэффициенты по складам
+        x = wb.get_coefficients()
 
-while True:
+        # если извлечение завершилось ошибкой, ждём 10 секунд и запускаем цикл по новой
+        if x != -1:
+            coefficients = json.loads(x.text)
+        else:
+            sleep(10)
+            continue
 
-    # извлекаем коэффициенты по складам
-    x = wb.get_coefficients()
+        # в лист result будем добавлять склады, которые нам подходят
+        result = []
+        for warehouse in warehouses:
+            get_warehouse(result, warehouse, coefficients)
 
-    # если извлечение завершилось ошибкой, ждём 10 секунд и запускаем цикл по новой
-    if x != -1:
-        coefficients = json.loads(x.text)
-    else:
-        sleep(10)
-        continue
-    
-    # в лист result будем добавлять склады, которые нам подходят
-    result = []
-    for warehouse in warehouses:
-        get_warehouse(result, warehouse, coefficients)
-                    
-    newlist = sorted(result, key=lambda d: d['warehouseName'])
+        current_list = sorted(result, key=lambda d: d['warehouseName'])
 
-    # оставляем склады, которые добавились на новой итерации 
-    result = []
-    for i in newlist:
-        if i not in old_list:
-            result.append(i)
+        # оставляем склады, которые добавились на новой итерации
+        result = []
+        for i in current_list:
+            if i not in previous_list:
+                result.append(i)
 
-    # отправляем сообщение пользователю
-    send_message(result)
+        # отправляем сообщение пользователю
+        send_message(result)
 
-    # запоминаем состояние для следующего цикла
-    old_list = newlist
+        # запоминаем состояние для следующего цикла
+        previous_list = current_list
 
-    # спим
-    sleep(60)
+        # спим
+        sleep(60)
+
+if __name__ == "__main__":
+    main()
--- wb.py
+++ wb.py
@@ -1,35 +1,34 @@
+'''API for WB'''
 import requests
-from time import sleep
 
-class wb:
+class WB:
+    '''Class for WB API'''
 
     def __init__(self, token) -> None:
         self.token = token # TOKEN доступ до API
 
     def get_coefficients (self):
-        
+        '''Get warehouse coefficients'''
         url = 'https://supplies-api.wildberries.ru/api/v1/acceptance/coefficients'
         headers = {'Authorization':self.token}
 
         try:
-            x = requests.get(url, headers=headers)
-        except:
-            print("Failed to get request")
-            return(-1)
-        else:
-            return(x)
-        
+            x = requests.get(url, headers=headers, timeout=10)
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something error",err)
+            return -1
+
+        return x
+
     def get_warehouses (self):
-        
+        '''Get list of warehouse'''
         url = 'https://supplies-api.wildberries.ru/api/v1/warehouses'
         headers = {'Authorization':self.token}
 
         try:
-            x = requests.get(url, headers=headers)
-        except:
-            print("Failed to get request")
-            return(-1)
-        else:
-            return(x)
-        
-            
+            x = requests.get(url, headers=headers, timeout=10)
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something error",err)
+            return -1
+
+        return x
